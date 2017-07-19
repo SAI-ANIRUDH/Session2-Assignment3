bf2f8ffff14e292c28b2c76d9172393b2c4b1cdd
SAI-ANIRUDH <SAI-ANIRUDH@users.noreply.github.com>
2017-07-19 11:43:43 +0530

    Update Assignment2_3.java

--- src/Assignment2_3.java
+++ src/Assignment2_3.java
@@ -1,14 +1,19 @@
+package assignment2;
 import java.util.*;
 public class Assignment2_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	for (int i = 1; i <=5; i++) {
-		for (int j=1;j<=5;j++) {
-			if (i==j) {
+	for (int i = 1; i <=5; i++) // Declaring conditions for outerloop for having number of rows as per the requirement// 
+	{
+		for (int j=1;j<=5;j++) // Declaring the conditions for innerloop related to columns as per the requirement//
+		{
+			if (i==j) // If rows value =column value then print * //
+			{
 				System.out.print("*");
 				}
-			if (i==1 && j== 5) {
+			if (i==1 && j== 5) 
+			{
 				System.out.print("*");
 			}
 			if (i==2 && j== 4) {
@@ -20,8 +25,9 @@ public class Assignment2_3 {
 			if (i==5 && j== 1) {
 				System.out.print("*");
 			}
-			else if(i+j ==5) {
-			System.out.print(" ");	
+			else if(i+j ==5) 
+			{
+			System.out.print(" ");	// Adding spaces //
 			}
 			else 
 				System.out.print("_");
